087f9b9a48d662fe1bb4bcdc3699071f6b8b2fca
naman <naman.gupta_cs18@gla.ac.in>
2020-04-24 22:58:20 +0530

    creating getter and setter for userPassword field

--- src/user/User.py
+++ src/user/User.py
@@ -13,5 +13,11 @@ class User:
     def get_userEmail(self):
         return self.userEmail
 
-    def set_userName(self, userEmail):
+    def set_userEmail(self, userEmail):
         self.userEmail = userEmail
+
+    def get_userPassword(self):
+        return self.userPassword
+
+    def set_userPassword(self, userPassword):
+        self.userPassword = userPassword
