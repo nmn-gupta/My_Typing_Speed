53097c7b33ec791079e52ebf64da1112c3889729
naman <naman.gupta_cs18@gla.ac.in>
2020-04-26 11:13:58 +0530

    completed method check_Password()

--- src/validate/Validation.py
+++ src/validate/Validation.py
@@ -4,7 +4,52 @@ class Validation:
         self.userPassword = userPassword
 
     def check_password(userPassword):
-        input_password = input("Enter the password: ")
+        s = userPassword
+        special = ",.#@$%&^*_-"
+        flag = 1
+        l = len(s)
+        if any(i.isspace() for i in s):
+            print("Inavalid Password!!")
+            print("There shouldn't be any space!!")
+            return False
+
+
+        elif not (l >= 5):
+            print("Inavalid Password!!")
+            print("Password must contain atleast five characters!!")
+            return False
+
+
+        elif not (l <= 10):
+            print("Inavalid Password!!")
+            print("Password contains atmost ten characters!!")
+            return False
+
+
+        elif not (any(i.isdigit() for i in s)):
+            print("Inavalid Password!!")
+            print("Password must contains atleast one digit!!")
+            return False
+
+        elif not (any(i in special for i in s)):
+            print("Inavalid Password!!")
+            print("Password must contain atleast one special character!!")
+            return False
+
+        elif not (any(i.isupper() for i in s)):
+            print("Inavalid Password!!")
+            print("Password must contain atleast one upper case alphabet!!")
+            return False
+
+
+        elif not (any(i.islower() for i in s)):
+            print("Inavalid Password!!")
+            print("Password must contain atleast one lower case alphabet!!")
+            return False
+
+
+        else:
+            return True
 
 
 obj_validation = Validation()
