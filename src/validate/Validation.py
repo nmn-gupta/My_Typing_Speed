450ec7a2fc8dc3de13019e83ff707ab37b5d4eab
naman <naman.gupta_cs18@gla.ac.in>
2020-04-26 11:10:39 +0530

    create the constructor for Validation class

--- src/validate/Validation.py
+++ src/validate/Validation.py
@@ -1,2 +1,28 @@
-def check_Email():
-    input_Email = input("Enter the Email: ")
+class Validation:
+    def __init__(self, userEmail, userPassword):
+        self.userEmail = userEmail
+        self.userPassword = userPassword
+
+
+obj_validation = Validation()
+
+
+def check_Email(userEmail):
+    e = userEmail
+    r = "abcdefghijklmnopqrstuvwxyz._0123456789/\*-"
+    if "@" in e:
+        t = e.partition("@")
+        if all(i in r for i in t[0]):
+            if all(j in r for j in t[2]) and (t[2].endswith(".com") or t[2].endswith(".in")):
+
+                return True
+
+
+            else:
+                print("Invalid Email!!")
+                return False
+
+    else:
+        print("Invalid Email!!")
+
+        return False
